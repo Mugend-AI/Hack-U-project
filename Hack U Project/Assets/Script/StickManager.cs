7c10cb5be2f17a373d81ded4968569c256b75b47
basiko-da-gama <bluesky.sunshine1997@gmail.com>
2020-08-29 17:12:15 +0900

    basiko1.2

--- Hack U Project/Assets/Script/StickManager.cs
+++ Hack U Project/Assets/Script/StickManager.cs
@@ -1,14 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class StickManager : MonoBehaviour
 {
+    private static readonly Joycon.Button[] m_buttons =
+    Enum.GetValues(typeof(Joycon.Button)) as Joycon.Button[];
+    private Joycon m_joyconR;
+    private Joycon.Button? m_pressedButtonR;
     private GameObject stickPosition;
     // Start is called before the first frame update
     void Start()
     {
         stickPosition = GameObject.FindWithTag("StickPosition");
+        m_joyconR = JoyconManager.Instance.j.Find(c => !c.isLeft);
     }
 
     // Update is called once per frame
@@ -22,5 +28,33 @@ public class StickManager : MonoBehaviour
         {
             transform.position = stickPosition.transform.position;
         }
+
+        m_pressedButtonR = null;
+
+        if (m_joyconR == null) return;
+
+        foreach (var button in m_buttons)
+        {
+            if (m_joyconR.GetButton(button))
+            {
+                m_pressedButtonR = button;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            m_joyconR.SetRumble(160, 320, 0.6f, 200);
+        }
+        var vec = m_joyconR.GetVector().eulerAngles;
+        var rb = gameObject.transform.localEulerAngles;
+        //rb.x += -gyro[1] * move;
+        //rb.y += -gyro[0] * move; 
+        rb.z = -vec.y + 180;
+        gameObject.transform.localEulerAngles = rb;
+        if (m_joyconR.GetButtonDown(Joycon.Button.DPAD_RIGHT))
+        {
+            gameObject.transform.localEulerAngles = Vector3.zero;
+        }
+
     }
 }

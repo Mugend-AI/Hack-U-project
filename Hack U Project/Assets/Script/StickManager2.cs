950fe9a8db2cb87f5aaf4f02932e10f9fdcd8f1f
basiko-da-gama <bluesky.sunshine1997@gmail.com>
2020-08-31 22:57:40 +0900

    basiko2.0

---
+++ Hack U Project/Assets/Script/StickManager2.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StickManager2 : MonoBehaviour
+{
+    private readonly float moveDistance = 0.01f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        /*
+        Vector3 firstPosi = Vector3.zero;
+        firstPosi.x = Random.Range(-3.0f, 3.0f);
+        firstPosi.z = Random.Range(-3.0f, 3.0f);
+        transform.position = firstPosi;
+        */       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 mv = Vector3.zero;
+        if (Input.GetAxis("HorizontalJoycon") >= 1)
+        {
+            mv.x = moveDistance;
+        }
+        if (Input.GetAxis("HorizontalJoycon") <= -1)
+        {
+            mv.x = -moveDistance;
+        }
+        if(Input.GetAxis("VerticalJoycon") >= 1)
+        {
+            mv.z = -moveDistance;
+        }
+        if(Input.GetAxis("VerticalJoycon") <= -1)
+        {
+            mv.z = moveDistance;
+        }
+        transform.Translate(mv);
+    }
+}

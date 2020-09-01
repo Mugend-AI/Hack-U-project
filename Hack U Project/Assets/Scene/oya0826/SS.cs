681254ad495742c6291cbe816cdd65bae6c340b3
Mugend-AI <69632988+Mugend-AI@users.noreply.github.com>
2020-09-01 17:16:02 +0900

    SSver1.1

---
+++ Hack U Project/Assets/Scene/oya0826/SS.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Android;
+using SocialConnector;
+using System;
+using System.IO;
+
+public class SS : MonoBehaviour
+{
+    public void ScreenShot()
+    {
+        StartCoroutine(TakeSS());
+    }
+
+    private IEnumerator TakeSS()
+    {
+        yield return new WaitForEndOfFrame();
+
+        var w = Screen.width;
+        var h = Screen.height;
+        var ss = new Texture2D(w, h, TextureFormat.RGB24, false);
+        ss.ReadPixels(new Rect(0, 0, w, h), 0, 0);
+        ss.Apply();
+
+        /*文字列のフォーマット指定*/
+        String fileName = String.Format("image_{0:yyyyMMdd_Hmmss}.png", DateTime.Now);
+
+        NativeGallery.SaveImageToGallery(ss, "ARMellon", fileName);
+
+        //String imagePath = Application.persistentDataPath + "/ARMellon/" + fileName;
+        //String imagePath = "/storage/emulated/0/DCIM/ARMellon/" + fileName;
+
+
+
+
+        string imgPath = Application.persistentDataPath + "/image.png";
+
+        // 前回のデータを削除
+        File.Delete(imgPath);
+
+        //スクリーンショットを撮影
+        ScreenCapture.CaptureScreenshot("image.png");
+
+        // 撮影画像の保存が完了するまで待機
+        while (true)
+        {
+            if (File.Exists(imgPath)) break;
+            yield return null;
+        }
+
+        // 投稿する
+        string tweetText = "てすと";
+        string tweetURL = "";
+        SocialConnector.SocialConnector.Share(tweetText, tweetURL, imgPath);
+
+
+
+    }
+}

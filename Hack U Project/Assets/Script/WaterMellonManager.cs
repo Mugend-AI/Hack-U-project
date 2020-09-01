9a48863cfff40b981304f920b675e4bd6c231f11
Mugend-AI <mugendai.9999@gmail.com>
2020-09-01 23:50:41 +0900

    kinoko ver5.1

--- Hack U Project/Assets/Script/WaterMellonManager.cs
+++ Hack U Project/Assets/Script/WaterMellonManager.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class WaterMellonManager : MonoBehaviour
 {
     [SerializeField] GameObject hiteffect;
+    [SerializeField] GameObject brokenMellon;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,7 +23,10 @@ public class WaterMellonManager : MonoBehaviour
         if (collision.gameObject.tag == "Stick") //StickSタグの付いたゲームオブジェクトと衝突したか判別
         {
             Instantiate(hiteffect, this.transform.position, Quaternion.identity); //パーティクル用ゲームオブジェクト生成
+            GetComponent<SphereCollider>().enabled = false;
+            GetComponent<MeshRenderer>().enabled = false;
             //Destroy(this.gameObject); //衝突したゲームオブジェクトを削除
+            Instantiate(brokenMellon);
             this.GetComponent<SS>().ScreenShot();
         }
     }

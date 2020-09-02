ead67a0b7a4edf5b2e2c00463f316cd7e4f9c426
Mugend-AI <69632988+Mugend-AI@users.noreply.github.com>
2020-09-02 17:22:33 +0900

    SEver1.1

---
+++ Hack U Project/Assets/Scene/oya0826/SE.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SE : MonoBehaviour
+{
+
+    public AudioClip sound;
+    AudioSource audioSource;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    public void Onbreak()
+    {
+
+        audioSource.PlayOneShot(sound);
+    }
+}
